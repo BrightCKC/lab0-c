69c81000ef4b45dccb55ce7ee6058b7332baaf05
Bright <q36111265@gs.nuku.edu.tw>
2024-02-29 13:55:31 +0800

    feat 實做 q_free

--- queue.c
+++ queue.c
@@ -26,7 +26,18 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l)
+        return;
+
+    struct list_head *node = NULL, *safe = NULL;
+    list_for_each_safe (node, safe, l) {
+        q_release_element(container_of(node, element_t, list));
+    }
+
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
